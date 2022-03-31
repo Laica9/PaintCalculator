19e640885cd9141b872b9353a8cf3e9811736c35
Ioanna <ioanna.kozeljp@gmail.com>
2022-03-31 12:50:27 +0100

    Modified calculator programme

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ConsoleApp1
 {
@@ -10,54 +11,64 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string intro = "Hi, I'm Jojo and I like anime.";
-            int year = 2022;
-            Console.WriteLine("\t" + intro + "\n" + "\tToday's date: 30th March " + year);
-            //Console.ReadKey();
-            //int result = (int)Math.Round(paintRequired); -- IGNORE
-
-            int Wall1_width = 1;
-            int depth = 2;
-            int height = 2;
-            int wallArea = Wall1_width * depth * height;
-
-            Console.WriteLine();    
-            Console.WriteLine("The surface area of the wall is: " + wallArea);
-
-            /*
-                Calculator to caluculate how much paint do I need 
-                to paint a wall surface area
-             */
-
-            Console.WriteLine("============================================");
-            Console.WriteLine("Please enter height of the wall: ");
-            string height1 = Console.ReadLine();
-            Console.WriteLine("Please enter length of the wall: ");
-            string length = Console.ReadLine();
-            Console.WriteLine("Please enter width of the wall: ");
-            string width1 = Console.ReadLine();
-
-            Console.WriteLine("\nHeight value of the wall entered ==> " + height1 + "m");
-            Console.WriteLine("\nLength value of the wall entered ==> " + length + "m");
-            Console.WriteLine("\nWidth value of the wall entered ==> " + width1 + "m");
-
-            int w = int.Parse(width1);
-            int h = int.Parse(height1);
-            int l = int.Parse(length);
-            double roomArea = 2 * (l + w) * h;
-            Console.WriteLine("\nTotal room area : " + roomArea + "m\u00b2");
-            Console.WriteLine("\n=======================================================");
-            // It is assumed there is 350 square feet per gallon
-            double squareftPerGallon = 350;
-            double numberOfGallons = roomArea / squareftPerGallon;
-            Console.WriteLine("\nGallons needed: " + numberOfGallons);
+            string program;
+            Console.WriteLine("Do you want to use my paint calculator? (Y/N)");
+            program = Console.ReadLine();
+            program = program.ToUpper();
 
 
-            // Paint required = pintable surface area / 10 (1L of paint will cover between
-            // 10 meters squared of wall. 
-            //double paintRequired = roomArea  / 10;
-            //double result = Math.Round(paintRequired,2);
-            //Console.WriteLine("Paint required around: " + result + " L");
+            while (program == "Y")
+            {
+                Console.WriteLine("Please enter length of the wall (in meters): ");
+                string length = Console.ReadLine();
+                Console.WriteLine("Please enter width of the wall (in meters): ");
+                string width = Console.ReadLine();
+                Console.WriteLine("Please enter height of the wall (in meters): ");
+                string height = Console.ReadLine();
+
+                double l = double.Parse(length);
+                double w = double.Parse(width);
+                double h = double.Parse(height);
+
+                if (l <= 0 || w <= 0 || h <= 0)
+                {
+                    Console.WriteLine("Sorry, Invalid integer.");
+                    Console.WriteLine(" Press R to restart the program...");
+                    string restart = Console.ReadLine();
+
+                    while(restart == "R")
+                    {
+                        
+                    }
+                }
+                else
+                {
+                    double wallArea = 2 * (l + w);
+                    Console.WriteLine("Total measurment: " + wallArea + "meters");
+                    double roomArea = wallArea * h;
+                    Console.WriteLine("Total area: " + roomArea + " square meters");
+
+                    Console.WriteLine("Please enter number of coats: ");
+                    string coat = Console.ReadLine();
+                    int c = int.Parse(coat);
+
+                    double squareMetersToBePainted = roomArea * c;
+                    Console.WriteLine("Total area to be painted: " + squareMetersToBePainted + " square meters");
+                    Console.WriteLine("Please enter paint capacity (per square meter): ");
+                    string paint = Console.ReadLine();
+                    int p = int.Parse(paint);
+                    double required = squareMetersToBePainted / p;
+                    double required1 = Math.Round(required, 2);
+                    Console.WriteLine("You would need " + required1 + " L of paint.");
+
+                    // required1 / container
+                }
+
+                Console.WriteLine("Would you like to use calculator again? (Y/N)");
+                program = Console.ReadLine();
+                program = program.ToUpper();
+            }   
+            
         }
     }
 }
